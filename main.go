909fb3cd1a0d317193623f6ac399f3285fd6dbeb
GeruIndu <indrajitmondal284@gmail.com>
2026-02-05 01:01:24 +0530

    First GET testing is done

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	application "github.com/GeruIndu/Golang/Application"
+)
+
+func main() {
+	app := application.New()
+
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("Failed to start error : ", err)
+	}
+}
